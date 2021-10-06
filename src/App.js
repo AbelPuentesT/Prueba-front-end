14f2bf7f5d04e536fde3221ecef0f509e265456d
AbelPuentesT <90673923+AbelPuentesT@users.noreply.github.com>
2021-10-06 11:52:00 -0500

    TodoForm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import react from 'react';
 import './App.css';
+import TodoForm from './components/TodoForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola mundo
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-app">
+      <h1>Todo app test</h1>
+      <TodoForm/>
+      
     </div>
   );
 }
