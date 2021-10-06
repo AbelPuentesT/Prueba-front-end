69c8ff0e468c9076cc632c768c67a008734459be
AbelPuentesT <90673923+AbelPuentesT@users.noreply.github.com>
2021-10-06 12:29:04 -0500

    TodoList

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,17 +1,30 @@
 import React, { useState } from "react";
 
-function TodoForm() {
+function TodoForm(props) {
   const [input, setInput] = useState("");
+  const handleChange = e => {
+      setInput(e.target.value);
+  };
+  const handleSubmit = e => {
+      e.preventDefault();
+      props.onSubmit({
+          id: Math.floor(Math.random() * 10000),
+          text: input
+      });
+      setInput("");
+  };
   return (
-    <form className="todo-form">
-      <imput
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <input
         type="text"
-        placeholder="Add a todo"
+        placeholder="Add a to-do activities"
         value={input}
         name="text"
         className="todo-input"
+        onChange={handleChange}
       />
-      <button className="todo-button">Add todo</button>
+      <button className="todo-button">Add
+      todo</button>
     </form>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
+import TodoForm from './TodoForm'
 
 function TodoList() {
+    const [todos,setTodos] =useState([]);
+    
+    const addTodo = todo => {
+        if (!todo.text || /^\s*$/.test(todo.text)) {
+          return;
+        }
+    
+        const newTodos = [todo, ...todos];
+    
+        setTodos(newTodos);
+        console.log(...todos);
+      };
     return (
         <div>
-            
+            <h1>To Do activities</h1>
+            <TodoForm onSubmit={addTodo}/>
         </div>
     )
 }
